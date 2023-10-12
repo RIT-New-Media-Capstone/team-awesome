17d0a976abbd34622bbf7b0d2b984c9fd90a2755
Dancetrap <85971084+Dancetrap@users.noreply.github.com>
2023-10-12 10:11:46 -0400

    Canvas Client Wireframe (#2)
    * canvas html setup
    
    * color functions
    
    ---------
    
    Co-authored-by: Hugo Serinese (RIT Student) <hhs2791@rit.edu>

--- main.js
+++ main.js
@@ -24,5 +24,12 @@
 
 let ctx = myCanvas.getContext("2d");
 
-ctx.fillStyle = "#c4d9ff";
+ctx.fillStyle = "#ffffff";
 ctx.fillRect(0, 0, 500, 400);
+
+function setColor(element) //set the color of the pen tool
+{
+    console.log(element.style.backgroundColor);
+
+    document.querySelector("body").style.backgroundColor = element.style.backgroundColor;
+}

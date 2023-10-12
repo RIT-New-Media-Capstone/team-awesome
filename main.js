ac9b0ace8f8cae39859fe5ce8acb063c1e4cce19
pkali076 <77549872+pkali076@users.noreply.github.com>
2023-10-12 13:45:59 -0400

    heroku'

--- main.js
+++ main.js
@@ -6,22 +6,22 @@
 
  // Your web app's Firebase configuration
  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
- const firebaseConfig = {
-   apiKey: "",
-   authDomain: "capstone-one-rit-team-awesome.firebaseapp.com",
-   projectId: "capstone-one-rit-team-awesome",
-   storageBucket: "capstone-one-rit-team-awesome.appspot.com",
-   messagingSenderId: "934984774408",
-   appId: "1:934984774408:web:da62ca55311bd4684eb2bd",
-   measurementId: "G-1LFVNP4WSQ"
- };
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "capstone-one-rit-team-awesome.firebaseapp.com",
+  projectId: "capstone-one-rit-team-awesome",
+  storageBucket: "capstone-one-rit-team-awesome.appspot.com",
+  messagingSenderId: "934984774408",
+  appId: "1:934984774408:web:da62ca55311bd4684eb2bd",
+  measurementId: "G-1LFVNP4WSQ"
+};
 
  // Initialize Firebase
- const app = initializeApp(firebaseConfig);
- const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
  let myCanvas = document.querySelector("#myCanvas");
-
 let ctx = myCanvas.getContext("2d");
 
 ctx.fillStyle = "#ffffff";
